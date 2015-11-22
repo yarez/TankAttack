2f84d9ad6dd8bb45f83130c42a600e46fd0427da
yarez <wbennett343434@gmail.com>
2015-11-22 16:25:55 -0500

    boop

---
+++ Source/TankAttack/TankAttackGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "TankAttackGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANKATTACK_API ATankAttackGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};

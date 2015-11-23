543a2cc50585c79b399e3e3bb17ea9b35993735e
chocokitty24 <foglek13@highpoint.edu>
2015-11-23 05:10:29 -0500

    Doot doot im a tank T__T

---
+++ Source/TankAttack/TankGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "TankGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANKATTACK_API ATankGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
